92ee9ae1e2059a1125b46a4bb508afa9ddd4b8c2
Narayan Iyengar <niyengar@gmail.com>
2026-01-11 10:08:04 -0800

    Initial Commit

---
+++ SahilStatsLite/SahilStatsLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahilStatsLiteApp.swift
+//  SahilStatsLite
+//
+//  Created by Narayan Iyengar on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SahilStatsLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
